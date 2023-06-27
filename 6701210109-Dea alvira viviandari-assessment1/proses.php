3ca47bce6d2092ca8efd8221145e92e2cf94e987
kemasghani <kemasghani123@gmail.com>
2023-06-27 12:38:58 +0700

    v1.3

---
+++ 6701210109-Dea alvira viviandari-assessment1/proses.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Get the input values from the form
+  $textbox1 = $_POST['textbox1'];
+  $textbox2 = $_POST['textbox2'];
+  $operation = $_POST['operation'];
+
+  // Perform the arithmetic operation based on the selected operation
+  if ($operation === '+') {
+    $result = $textbox1 + $textbox2;
+  } elseif ($operation === '-') {
+    $result = $textbox1 - $textbox2;
+  } elseif ($operation === '*') {
+    $result = $textbox1 * $textbox2;
+  } elseif ($operation === '/') {
+    $result = $textbox1 / $textbox2;
+  } else {
+    $result = 'Invalid operation';
+  }
+
+  // Display the result
+  echo "Result: $result";
+}
